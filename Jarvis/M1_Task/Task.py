bca90fe6ba3ed7bec90e469b3074a48b145a7a23
parthdhwajendramishra <parthdhwajendramishra@gmail.com>
2020-01-03 21:51:18 +0530

    Prescription Module Modified

--- Jarvis/M1_Task/Task.py
+++ Jarvis/M1_Task/Task.py
@@ -1,8 +1,9 @@
 import datetime
 import sys  
-sys.path.append('D:\Project\Jarvis_Assitant')  
+sys.path.append('D:\Project Git Hub\Jarvis_Assistant\Jarvis')  
 from M2_VTTV.VTTV import text_voice as v
 from M3_Pwriter.fwrite import file_op as fop
+from M3_Pwriter.formatter import format as fw
 
 class task:
     def execute(query):
@@ -12,9 +13,9 @@ class task:
             
         elif 'cription' in query:
             v.speak("Please speak your contents")
-            query = v.takeCommand()
-            fop.write_text(query)
-            
+            #query = v.takeCommand()
+            #fop.write_text(query)
+            fw.set_format()
         else:
             v.speak("Sorry! I didn't get anything.")
             
---
+++ Jarvis/M3_Pwriter/formatter.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.append('D:\Project Git Hub\Jarvis_Assistant\Jarvis')  
+from M2_VTTV.VTTV import text_voice as v
+
+class format:
+    def set_format():
+        info={}
+        v.speak("Hello Doctor, what is Patient name")
+        info.update({"Name":v.takeCommand().lower()})
+
+        v.speak("What is his age")
+        info.update({"Age":v.takeCommand().lower()})
+
+        v.speak("What are the Symptons")
+        info.update({"Problem":v.takeCommand().lower()})
+
+        v.speak("What are the Diagnosis")
+        info.update({"Diagnosis":v.takeCommand().lower()})
+
+        v.speak("What are the Prescriptions")
+        info.update({"Prescriptions":v.takeCommand().lower()})
+
+        v.speak("Any advice doctor")
+        info.update({"advice":v.takeCommand().lower()})
+
+        print(str(info))
+
+
+
+
