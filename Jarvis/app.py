c7bc6b13864229b5d5e87deaf7b1c8aa618ddf33
parthdhwajendramishra <parthdhwajendramishra@gmail.com>
2020-01-03 23:32:23 +0530

    Changes in tha app file

--- Jarvis/app.py
+++ Jarvis/app.py
@@ -3,10 +3,10 @@ from M2_VTTV.VTTV import text_voice as v
 from M1_Task.Task import task as tsk
 
 if __name__ == "__main__":
-   # gt.wishMe()
     while True:
-        ans = v.takeCommand().lower()
-        if  'jarvis' in ans :
+        query = v.takeCommand().lower()
+        if  'jarvis' in query or 'hello' in query:
+          gt.wishMe()
           query = v.takeCommand().lower()
           tsk.execute(query)
         
