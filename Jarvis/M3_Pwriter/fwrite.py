0b146f2e2bfeeccab42598ba3cd5bb0819f1976a
preetichand <preetichand40@gmail.com>
2020-01-04 14:34:42 +0530

    m3

--- Jarvis/M3_Pwriter/fwrite.py
+++ Jarvis/M3_Pwriter/fwrite.py
@@ -3,6 +3,7 @@ class file_op:
     myfile = open('doc.txt', 'w')
     for x, y in query.items():
       myfile.write(x+':'+y+'\n')
+      myfile.write('Date :%s'%(dt.date.today()))
     myfile.close()  
 
 
